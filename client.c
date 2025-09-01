adc69d3c4f305eb0f3137aeb0243cb722f23961f
andrewzshen <andrewshenrocks@gmail.com>
2025-09-01 01:33:55 -0700

    implemented back and forth communication

--- client.c
+++ client.c
@@ -12,11 +12,11 @@
 #define IP_ADDRESS "127.0.0.1"
 
 int main(int argc, char **argv) {
-    int client_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-    if(client_fd == -1) {
-        perror("Socket creation failed.");
-        return 1;
+    if(client_socket == -1) {
+        perror("Socket creation failed");
+        exit(errno);
     }
 
     struct sockaddr_in server_addr;
@@ -25,24 +25,42 @@ int main(int argc, char **argv) {
     inet_pton(AF_INET, IP_ADDRESS, &server_addr.sin_addr);
     memset(&server_addr.sin_zero, 0, sizeof(server_addr.sin_zero));
 
-    if(connect(client_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1) {
-        perror("Connect failed.");
-        close(client_fd);
-        return 1;
+    if(connect(client_socket, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1) {
+        perror("Connect failed");
+        close(client_socket);
+        exit(errno);
     }
 
+    printf("Connected to server. Input messages:\n");
+
     char buffer[1024];
-    ssize_t bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);  
+    ssize_t bytes_sent;
 
-    if(bytes_received == -1) {
-        perror("Receive failed.");
-        close(client_fd);
-        return 1;
-    } 
+    while(fgets(buffer, sizeof(buffer), stdin) != NULL) {
+        size_t newline_index = strcspn(buffer, "\n");
+        buffer[newline_index] = 0;
 
-    printf("Received: %s", buffer);
+        printf("Message length in bytes (excluding newline): %d\n", strlen(buffer));
+        bytes_sent = send(client_socket, buffer, strlen(buffer), 0);
 
-    close(client_fd);
+        if(bytes_sent == -1) {
+            perror("Send error.");
+            close(client_socket);
+            exit(errno);
+        }
+
+        ssize_t bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);  
+
+        if(bytes_received == -1) {
+            perror("Receive failed");
+            close(client_socket);
+            exit(errno);
+        } 
+
+        printf("%.*s\n", bytes_received, buffer);
+    }
+
+    close(client_socket);
 
     return 0;
 }   
--- server.c
+++ server.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,18 +14,18 @@
 #define IP_ADDRESS "127.0.0.1"
 
 int main(int argc, char **argv) {
-    int server_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-    if(server_fd == -1) {
-        perror("Socket creation failed.");
-        return 1;
+    if(server_socket == -1) {
+        perror("Socket creation failed");
+        exit(errno);
     }
 
-    int opt = 1;
-    if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt)) == -1) {
-        perror("Set SO_REUSEADDR failed.");
-        close(server_fd);
-        return 1;
+    int option = 1;
+    if(setsockopt(server_socket, SOL_SOCKET, SO_REUSEPORT, &option, sizeof(option)) == -1) {
+        perror("Set SO_REUSEADDR failed");
+        close(server_socket);
+        exit(errno);
     }
 
     struct sockaddr_in server_addr;
@@ -32,46 +34,72 @@ int main(int argc, char **argv) {
     inet_pton(AF_INET, IP_ADDRESS, &server_addr.sin_addr);
     memset(&server_addr.sin_zero, 0, sizeof(server_addr.sin_zero));
 
-    if(bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1) {
-        perror("Bind failed.");
-        close(server_fd);
-        return 1;
+    if(bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1) {
+        perror("Bind failed");
+        close(server_socket);
+        exit(errno);
     }
 
     int connection_backlog = 10;
 
-    if(listen(server_fd, connection_backlog) == -1) {
-        perror("Listen failed.");
-        close(server_fd);
-        return 1;
+    if(listen(server_socket, connection_backlog) == -1) {
+        perror("Listen failed");
+        close(server_socket);
+        exit(errno);
     }
 
     printf("Waiting for a client to connect...\n");
 
     struct sockaddr_in client_addr;
     socklen_t client_addr_len = sizeof(client_addr);
-    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+    int client_socket = accept(server_socket, (struct sockaddr *) &client_addr, &client_addr_len);
 
-    if(client_fd == -1) {
-        perror("Accept failed.");
-        close(server_fd);
-        return 1;
+    if(client_socket == -1) {
+        perror("Accept failed");
+        close(client_socket);
+        close(server_socket);
+        exit(errno);
     }
 
-    printf("Client connected.\n");
+    printf("Client connected. Ready to receive messages.\n");
 
-    char *message = "Hello client!\n";
-    ssize_t bytes_sent = send(client_fd, message, strlen(message), 0);
+    const char *response_prefix = "Received message from client! Message: ";
+    const size_t prefix_len = strlen(response_prefix);
 
-    if(bytes_sent == -1) {
-        perror("Send failed.");
-        close(client_fd);
-        close(server_fd);
-        return 1;
+    // Because the initial response prefix is always known at compile time, it is memcpy'ed into a buffer
+    // and then the actual message you want to echo is recv into the same buffer just starting after the prefix
+    size_t buffer_size = 1024;
+    char buffer[buffer_size];
+    memcpy(buffer, response_prefix, prefix_len);
+
+    ssize_t bytes_received;
+
+    // Only receive buffer_size - prefix_len bytes because the buffer is of size buffer_size, so the maximum number
+    // number of bytes you can receive is their difference 
+    while((bytes_received = recv(client_socket, buffer + prefix_len, buffer_size - prefix_len, 0)) > 0) {
+        ssize_t bytes_sent = send(client_socket, buffer, prefix_len + bytes_received, 0); 
+
+        if(bytes_sent == -1) {
+            perror("Send failed");
+            close(server_socket);
+            close(client_socket);
+            exit(errno);
+        }
+
+        printf("Message received. Response: '%.*s'\n", bytes_sent, buffer);
     }
 
-    close(client_fd);
-    close(server_fd);
+    if(bytes_received == -1) {
+        perror("Receive failed");
+        close(client_socket);
+        close(server_socket);
+        exit(errno);
+    }
+
+    printf("Connection closed.\n");
+
+    close(client_socket);
+    close(server_socket);
 
     return 0;
 }   
